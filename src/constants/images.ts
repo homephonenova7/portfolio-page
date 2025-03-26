6c229f68c326e56697b60f40d3b8d2c662f461eb
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-03-26 22:47:08 +0530

    Update images.ts

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,10 @@
 const images = {
   booktogothumb: "/images/projects/BookToGo.lk/thumb.png",
   skillbridgethumb: "/images/projects/SkillBridge/thumb.png",
+  sddsthumb: "/images/projects/sdds apo/thumb.png",
+  stichingguide : "/images/projects/stitching-guide.jpg",
+  skillbridgereact : "/images/projects/SkillBridge/react.png",
+  skillbridhenext : "/images/projects/SkillBridge/next.png",
 };
 
 export default images;
